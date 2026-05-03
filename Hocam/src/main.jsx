0a6a768f56dcbf4648cb78d83e3db92b3081c41f
Əli Muxtarlı <alimukhtarli085@gmail.com>
2026-05-03 06:48:13 +0400

    Section 3 added on main.jsx

--- Hocam/src/main.jsx
+++ Hocam/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import React, { useState, useEffect } from "react";
 import  MarketingHome from "./MarketingHome"
 import MarketingSection2 from "./MarketingSection2";
-
+import MarketingSection3 from "./MarketingSection3"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -10,5 +10,6 @@ root.render(
   <div>
   <MarketingHome />
   <MarketingSection2 />
+  <MarketingSection3 />
   </div>
 );
